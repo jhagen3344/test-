b19ec0cf4c05b0b3fa9dca5de9d9150b01b6fc5c
jhagen3344 <37031308+jhagen3344@users.noreply.github.com>
2018-03-11 15:12:41 -0400

    update

--- hidden.php
+++ hidden.php
@@ -1,8 +1,24 @@
-<?php 
-//Edit Here To Change Password 
-if (@$_GET['password'] == 1234) {
-?>correct<?php 
-// else Conditional region1
-} else { ?>Wrong<?php } 
-// endif Conditional region1
+<?php
+
+//CHECK IF THE GET REQUEST IS EMPTY
+if (empty($_GET['password'])){
+	
+	// IF EMPTY ECHO OUT NO PASSWORD
+    echo 'No Password Detected';
+	
+//ELSE IF GET IS NOT EMPTY
+}else{
+	// SET VERIABLE $PASSWORD TO THE CONTENTS OF GET
+	$Password = $_GET['password'];
+	
+	// IF THE VERIABLE PASSWORD = DEFAULT PASSWORD 
+	if($password == '3263'){
+		// ECHO OUT CORRECT AS PASSWORD MATCH
+		echo 'correct';
+	}else{
+		// IF PASSWORDS DONT MATCH THEN ECHO OUT WRONG
+		echo 'Wrong';
+	}
+	
+}
 ?>
