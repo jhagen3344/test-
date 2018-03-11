b2138a3dcdcb614f70efaa42d4df456692690683
jhagen3344 <37031308+jhagen3344@users.noreply.github.com>
2018-03-11 11:28:25 -0400

    et

---
+++ hidden.php
@@ -0,0 +1,8 @@
+<?php 
+//Edit Here To Change Password 
+if (@$_GET['password'] == 123456) {
+?>correct<?php 
+// else Conditional region1
+} else { ?>Wrong<?php } 
+// endif Conditional region1
+?>
